c8f01bbb2d858f19472580c1b581fa261d1b4f63
atenatiwosgetachew1-ET <atenatiwosgetachew1@gmail.com>
2026-09-07 14:52:29 +0000

    chore: enable pristine 500-record single batch test

--- service_factory/services/Linkx_xmaintenance/src/linkx_xvigilance/fetcher.py
+++ service_factory/services/Linkx_xmaintenance/src/linkx_xvigilance/fetcher.py
@@ -60,6 +60,7 @@ def stream_window_records(
             records = [hit.get('_source', {}) for hit in hits]
             if records:
                 yield records[:500]
+                break[:500]
                 break
             
             # Fetch next batch
